9edeb57bd889be36a91daece69c687d3874a00ec
Rubal <rahmed2210@gmail.com>
2023-05-23 16:09:16 -0400

    Local Scope and Functions

--- localScopeAndFunctions.js
+++ localScopeAndFunctions.js
@@ -1,7 +1,7 @@
-/**
-                          Local Scope and Functions
+/**       ----->  Local Scope and Functions  <-----
 
-Variables which are declared within a function, as well as the function parameters, have local scope. That means they are only visible within that function.
+Variables which are declared within a function, as well as the function parameters, 
+have local scope. That means they are only visible within that function.
 
 Here is a function myTest with a local variable called loc.
 
@@ -12,11 +12,17 @@ function myTest() {
 
 myTest();
 console.log(loc);
-The myTest() function call will display the string foo in the console. The console.log(loc) line (outside of the myTest function) will throw an error, as loc is not defined outside of the function.
 
-The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. Declare a local variable myVar inside myLocalScope and run the tests.
+The myTest() function call will display the string foo in the console. 
+The console.log(loc) line (outside of the myTest function) will throw an error, 
+as loc is not defined outside of the function.
 
-Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail.
+The editor has two console.logs to help you see what is happening. Check the 
+console as you code to see how it changes. Declare a local variable myVar 
+inside myLocalScope and run the tests.
+
+Note: The console will still display ReferenceError: myVar is not defined, 
+but this will not cause the tests to fail.
 
 */
 
