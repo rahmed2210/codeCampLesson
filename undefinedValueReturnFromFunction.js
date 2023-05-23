b305b1555160204d0532685a55d09ce6972490a2
Rubal <rahmed2210@gmail.com>
2023-05-23 16:10:24 -0400

    Understanding Undefined Value returned from a Function

--- undefinedValueReturnFromFunction.js
+++ undefinedValueReturnFromFunction.js
@@ -1,6 +1,7 @@
-/*                            Understanding Undefined Value returned from a Function
+/*      -----> Understanding Undefined Value returned from a Function  <-----
                  
-A function can include the return statement but it does not have to. In the case that the function doesn't have a return statement, when you call it, 
+A function can include the return statement but it does not have to. In the case 
+that the function doesn't have a return statement, when you call it, 
 the function processes the inner code but the returned value is undefined.
 
 Example
@@ -12,9 +13,11 @@ function addSum(num) {
 }
 
 addSum(3);
-addSum is a function without a return statement. The function will change the global sum variable but the returned value of the function is undefined.
+addSum is a function without a return statement. The function will change the 
+global sum variable but the returned value of the function is undefined.
 
-Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+Create a function addFive without any arguments. This function adds 5 to 
+the sum variable, but its returned value is undefined.
 */
 
 // addFive should be a function.
