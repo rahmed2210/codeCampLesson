57588264fceeb9a625157c671bae073fa29eedda
Rubal <rahmed2210@gmail.com>
2023-05-23 16:02:36 -0400

    Local Scope and Functions

--- GlobalScopeandFunctions.js
+++ GlobalScopeandFunctions.js
@@ -1,9 +1,15 @@
 /*
-In JavaScript, scope refers to the visibility of variables. Variables which are defined outside of a function block have Global scope. This means, they can be seen everywhere in your JavaScript code.
+In JavaScript, scope refers to the visibility of variables. Variables which are 
+defined outside of a function block have Global scope. This means, they can be seen 
+everywhere in your JavaScript code.
 
-Variables which are declared without the let or const keywords are automatically created in the global scope. This can create unintended consequences elsewhere in your code or when running a function again. You should always declare your variables with let or const.
+Variables which are declared without the let or const keywords are automatically 
+created in the global scope. This can create unintended consequences elsewhere in 
+your code or when running a function again. You should always declare your variables 
+with let or const.
 
-Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+Using let or const, declare a global variable named myGlobal outside of any function. 
+Initialize it with a value of 10.
 
 Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
 */
