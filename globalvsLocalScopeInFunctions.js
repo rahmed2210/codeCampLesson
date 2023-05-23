ffec840a978018437631f9ddcee7edaf453c5b61
Rubal <rahmed2210@gmail.com>
2023-05-23 16:07:53 -0400

    Global vs. Local Scope in Functions

--- globalvsLocalScopeInFunctions.js
+++ globalvsLocalScopeInFunctions.js
@@ -1,6 +1,7 @@
-/**
- lobal vs. Local Scope in Functions
-It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
+/**     ------> Global vs. Local Scope in Functions  <-----
+ 
+It is possible to have both local and global variables with the same name. 
+When you do this, the local variable takes precedence over the global variable.
 
 In this example:
 
@@ -10,9 +11,12 @@ function myFun() {
   const someVar = "Head";
   return someVar;
 }
-The function myFun will return the string Head because the local version of the variable is present.
 
-Add a local variable to myOutfit function to override the value of outerWear with the string sweater. 
+The function myFun will return the string Head because the local version of the 
+variable is present.
+
+Add a local variable to myOutfit function to override the value of outerWear with 
+the string sweater. 
 */
 
 // Waiting:You should not change the value of the global outerWear.
